bf662d940a5f37dd5535ffcaa62e7176b4df58c3
nvrr <sureshvarma809@gmail.com>
2020-07-30 10:03:36 +0530

    Week-2 Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,14 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-import { DishService } from './services/dish.service';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
-import { AppRoutingModule } from './app-routing/app-routing.module'
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,
+              PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
