e32768ad766d89abf8d7de13232e67940bfe2a9b
nvrr <sureshvarma809@gmail.com>
2020-08-14 12:22:43 +0530

    Weeek-4 Animations Part-2: CustomeAnimations

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,26 +4,19 @@ import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
+import { visibility, flyInOut, expand } from '../animations/app.animation';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-     trigger('visibility',[
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-             transform: 'scale(0.5)',
-             opacity: 0
-        })),
-        transition('* => *', animate('1s ease-in-out'))
-     ])
+     visibility(),
+     flyInOut(),
+     expand()
   ]
 })
 export class DishdetailComponent implements OnInit {
